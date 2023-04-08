253b5645152eddbb1a38b536e896747d53f4ee3a
cleyton.cabral <cleyton.cabral@ferreiracosta.com.br>
2023-04-08 14:08:53 -0300

    Adição de pasta styles e criação de theme.

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import { Routers } from './Routers';
-import { GlobalStyle } from './styles';
+import { GlobalStyle } from './styles/global-styles';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
