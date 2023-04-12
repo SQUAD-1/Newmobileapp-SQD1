cd0e108fd83d5182d10fff4ab9578b70b96d2402
pedro.souza <pedro.souza@ferreiracosta.com.br>
2023-04-12 20:55:40 -0300

    Criação do componente de input

---
+++ src/Assets/midiadoc.tsx
@@ -0,0 +1,56 @@
+export const MidiaDoc = () => {
+    return (
+        <svg
+            width="80"
+            height="80"
+            viewBox="0 0 80 80"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+        >
+            <rect width="80" height="80" rx="4" fill="#EBF6E3" />
+            <mask
+                id="mask0_0_1"
+                style={{ maskType: "alpha" }}
+                maskUnits="userSpaceOnUse"
+                x="15"
+                y="15"
+                width="50"
+                height="50"
+            >
+                <rect
+                    x="15.6099"
+                    y="15.6098"
+                    width="48.7805"
+                    height="48.7805"
+                    fill="url(#pattern0)"
+                />
+            </mask>
+            <g mask="url(#mask0_0_1)">
+                <rect
+                    x="15.6099"
+                    y="15.6098"
+                    width="48.7805"
+                    height="48.7805"
+                    fill="#7AC143"
+                />
+            </g>
+            <defs>
+                <pattern
+                    id="pattern0"
+                    patternContentUnits="objectBoundingBox"
+                    width="1"
+                    height="1"
+                >
+                    <use xlinkHref="#image0_0_1" transform="scale(0.0104167)" />
+                </pattern>
+                <image
+                    id="image0_0_1"
+                    width="96"
+                    height="96"
+                    xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGAAAABgCAYAAADimHc4AAAACXBIWXMAAAsTAAALEwEAmpwYAAAB70lEQVR4nO2dQU4CURBEayF3UU7p0kQUTqOeQ/EgijtMypCwcqNkYLp+db2klyRV/eZ/ZjdACCGEEEIIIUxnCWANYAvgCwCL57aL1AWADYBvgaWzm4TD8l8EFs2uEjYCC2ZXCUvRa4ddJKwFlsoT5w5GvAsslJ1Pwk5gmex8Ev4qqZaHbhJGF8DRJTgI4MgSXARwVAlOAjiiBDcBHE2CowCOJMFVAEeR4CyAI0hwF0B1CR0EUFlCFwFUldBJABUldBNANQkdBVBJQlcBVJHQWQAhgFpARkAEzEpOQDFqAtr1kw/o3k8+oHs/+YDu/eQDuveTD+jeTz6gez/5gO795AO695MP6N5PPqB7P/mA7v2mBuSJv+eZ59L9Lk4EFBMBxURAMRFQTAQUEwHFREAx8u/J7v3kA7r3kw/o3k8+oHs/+YDu/eQDuveTD+jeTz6ge7+pAXnmmTtfORFQTAQUEwHFREAxEVBMBBQTAcXIvye795MP6N5PPqB7P/mA7v3kA7r3kw/o3k8+oHs/+YDu/T7/EdJ1PiDAVmARLJpXCPAosAgWzT0EuAGwF1gGZ55D52uIMOKnrDhxHiD2KcNngaVwpnkCcAUxFseT4Hwd7Y9Pvtzyf/8nrAC8Df6NMR5nd+yyUrrzQwghhBBCCBiWH/Em/KCwAPmIAAAAAElFTkSuQmCC"
+                />
+            </defs>
+        </svg>
+    );
+};
