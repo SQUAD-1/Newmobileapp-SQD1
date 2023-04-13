ac241a49b7cb34511563d39bb91f8218381250d1
camylla.lima <camylla.lima@ferreiracosta.com.br>
2023-04-13 15:25:40 -0300

    Tela de Home (faltando o menu de navegação)

---
+++ src/Components/Home/ButtonNew/styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const ButtonNewCalled = styled.button`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 0;
+    width: 5rem;
+    height: 5rem;
+    background-color: #ebf6e3;
+    box-shadow: 0px 4px 8px 3px rgba(0, 0, 0, 0.15),
+        0px 1px 3px rgba(0, 0, 0, 0.3);
+    border-radius: 16px;
+    cursor: pointer;
+
+    position: absolute;
+    bottom: 5rem;
+`;
--- src/Routers.tsx
+++ src/Routers.tsx
@@ -3,11 +3,13 @@ import { Chamados } from "./pages/Chamados";
 import { AbrirChamado } from "./pages/Chamados/AbrirChamado";
 import { ChamadoScreen } from "./pages/Chamados/TelaDoChamado";
 import { ConfirmacaoScreen } from "./pages/Chamados/TelaDeConfirmação";
+import { Home } from "./pages/Home";
 
 export const Routers = () => {
     return (
         <Routes>
             <Route path="/" element={<AbrirChamado />} />
+            <Route path="/home" element={<Home />} />
             <Route path="/teladochamado" element={<ChamadoScreen />} />
             <Route path="/teladeconfirmação" element={<ConfirmacaoScreen />} />
         </Routes>
