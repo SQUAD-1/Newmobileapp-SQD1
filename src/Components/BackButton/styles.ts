ac117a766642c38bf4628554d7a1da834ceca532
ErikaCibellySx24 <105865954+ErikaCibellySx24@users.noreply.github.com>
2023-04-30 19:53:48 -0300

    Correção do peso do botão voltar

--- src/Components/BackButton/styles.ts
+++ src/Components/BackButton/styles.ts
@@ -9,14 +9,14 @@ export const Container = styled.div`
 `;
 
 export const ImgBackIcon = styled.img`
-    width: 11px;
-    height: 15px;
+   // width: 11px;
+    height: 24px;
     margin-top: 2px;
 `;
 export const TextBack = styled.span`
     font-family: "Inter";
     font-style: normal;
-    font-weight: 400;
+    font-weight: 500;
     font-size: 2rem;
     display: flex;
     align-items: center;
