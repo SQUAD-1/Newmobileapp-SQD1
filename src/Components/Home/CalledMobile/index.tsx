3ac917a8519767da64a0fd2bcda546b6291487f9
Wellington Braga <wellington.braga@ferreiracosta.com.br>
2023-05-06 18:57:48 -0300

    Meu deus dnv

--- src/Components/Home/CalledMobile/index.tsx
+++ src/Components/Home/CalledMobile/index.tsx
@@ -1,41 +1,41 @@
 import {
-    CalledContainer,
-    CalledContent,
-    CalledNumber,
-    CalledDescription,
-    CalledState,
-    CalledStatus,
-    OpeningText,
-    StatusText,
-    IconeSelo,
+	CalledContainer,
+	CalledContent,
+	CalledNumber,
+	CalledDescription,
+	CalledState,
+	CalledStatus,
+	OpeningText,
+	StatusText,
+	IconeSelo,
 } from "./styles";
 import { Selo } from "../../../Assets/Icons";
 import { CalledMobileProps } from "../../../Assets";
 export const CalledMobile = ({
-    color,
-    callNumber,
-    callDescription,
-    callDate,
-    callStatus,
-    updated,
+	color,
+	callNumber,
+	callDescription,
+	callDate,
+	callStatus,
+	updated,
 }: CalledMobileProps) => {
-    return (
-        <CalledContainer color={color}>
-            <IconeSelo>{updated ? <Selo /> : null}</IconeSelo>
-            <CalledContent>
-                <CalledNumber>{callNumber}</CalledNumber>
-                <CalledDescription>{callDescription}</CalledDescription>
-            </CalledContent>
-            <CalledState>
-                <CalledStatus>
-                    Aberto em:
-                    <OpeningText>{callDate}</OpeningText>
-                </CalledStatus>
-                <CalledStatus>
-                    Status
-                    <StatusText>{callStatus}</StatusText>
-                </CalledStatus>
-            </CalledState>
-        </CalledContainer>
-    );
+	return (
+		<CalledContainer color={color}>
+			<IconeSelo>{updated ? <Selo /> : null}</IconeSelo>
+			<CalledContent>
+				<CalledNumber>{callNumber}</CalledNumber>
+				<CalledDescription>{callDescription}</CalledDescription>
+			</CalledContent>
+			<CalledState>
+				<CalledStatus>
+					Aberto em:
+					<OpeningText>{callDate}</OpeningText>
+				</CalledStatus>
+				<CalledStatus>
+					Status
+					<StatusText>{callStatus}</StatusText>
+				</CalledStatus>
+			</CalledState>
+		</CalledContainer>
+	);
 };
