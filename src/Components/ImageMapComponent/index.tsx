b9b2a3f5938bfce87eca89e1f05144758bdd936f
Cleyton Cabral <cleyton.cabral@ferreiracosta.com.br>
2023-05-10 10:31:09 -0300

    updates

--- src/Components/ImageMapComponent/index.tsx
+++ src/Components/ImageMapComponent/index.tsx
@@ -3,6 +3,7 @@ import {
 	ButtonAction,
 	ButtonActionContainer,
 	FileContainer,
+	IconCloseContent,
 	ImageMapContainer,
 	Title,
 	TypeFile,
@@ -10,6 +11,7 @@ import {
 // import iconFileImage from "./svg/background.svg";
 import iconRestart from "./svg/Restart.svg";
 import iconTrash from "./svg/Trash.svg";
+import close from "./svg/Close.svg";
 
 interface ImageMapComponentProps {
 	img: File;
@@ -27,12 +29,17 @@ export const ImageMapComponent = ({ img }: ImageMapComponentProps) => {
 	}, [setOpenImage]);
 
 	const image = URL.createObjectURL(img as unknown as Blob);
-	console.log("text", img.name);
 
 	return (
 		<ImageMapContainer>
 			<FileContainer onClick={() => setOpenImage(true)}>
 				<TypeFile showImage={openImage}>
+					<IconCloseContent onClick={() => setOpenImage(false)}>
+						<img
+							src={close}
+							alt="Fechar imagem"
+						/>
+					</IconCloseContent>
 					<img
 						src={image}
 						width={350}
