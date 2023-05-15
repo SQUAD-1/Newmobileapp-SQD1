ae75ac2e384525fea0d9512355ea069cecbc8d32
Larissa Ferreira <larissa.ferreira@ferreiracosta.com.br>
2023-05-15 08:39:15 -0300

    otimização do código

--- src/Pages/Chamados/AbrirChamado/index.tsx
+++ src/Pages/Chamados/AbrirChamado/index.tsx
@@ -56,10 +56,10 @@ export const AbrirChamado = () => {
 						selected>
 						Qual o tipo do chamado?
 					</option>
-					{typeCall.map((tipo, index) => (
+					{typeCall.map((tipo) => (
 						<option
 							value={tipo.type}
-							key={index}>
+							key={tipo.id}>
 							{tipo.type}
 						</option>
 					))}
