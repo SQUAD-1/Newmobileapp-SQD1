8158320b8393cd3c603d41a03498b0bbd1e0ffa5
José Mateus <mateus.silva@ferreiracosta.com.br>
2023-05-16 14:02:37 -0300

    corrigindo propriedade width

--- src/Pages/Chamados/AbrirChamado/index.tsx
+++ src/Pages/Chamados/AbrirChamado/index.tsx
@@ -51,7 +51,8 @@ export const AbrirChamado = () => {
 					placeholder="Do que se trata o chamado?"
 					height="56px"
 					width="auto"
-					width="auto"
+					getValue={(e)=> {e;}}
+
 				/>
 				<SelectOption
 					legendText="Tipo"
@@ -79,7 +80,8 @@ export const AbrirChamado = () => {
 					placeholder="Nos conte mais detalhes sobre o ocorrido..."
 					height="240px"
 					width="auto"
-					width="auto"
+					getValue={(e)=> {e;}}
+
 				/>
 				<InputLegend
 					legendText="Data do ocorrido"
