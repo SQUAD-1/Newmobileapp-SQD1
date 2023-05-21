6fee3a7b1736c2e25b03fdc7b8138d4979e41aae
Larissa Ferreira <larissa.ferreira@ferreiracosta.com.br>
2023-05-21 16:24:52 -0300

    Alterações em alguns componentes

---
+++ src/Pages/NewPassword/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Title = styled.div`
+	font-family: Inter;
+	font-style: normal;
+	font-size: 16px;
+	font-weight: 600;
+	line-height: 24px;
+	letter-spacing: 0.01em;
+	text-align: left;
+	color: #5a8f19;
+`;
