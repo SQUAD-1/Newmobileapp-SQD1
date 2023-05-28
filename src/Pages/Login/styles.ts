4f05465bf4f47db2ec9a7e61cf9c15c56ab9a66c
Larissa <96215793+imlari@users.noreply.github.com>
2023-05-28 13:42:47 -0300

    Update styles.ts

--- src/Pages/Login/styles.ts
+++ src/Pages/Login/styles.ts
@@ -109,7 +109,7 @@ export const Logo = styled.div`
 export const LoginMobile = styled.div`
 	display: none;
 	width: 100%;
-	height: 100vh;
+	height: 100dvh;
 	padding: 0px 20px;
 	background-color: #f8fcf6;
 `;
