fb37c6789f907ee6741bb102387e643b1758167c
camylla.lima <camylla.lima@ferreiracosta.com.br>
2023-05-29 12:29:31 -0300

    Conexão com api

--- src/Pages/ForgotPassword/NewPassword/index.tsx
+++ src/Pages/ForgotPassword/NewPassword/index.tsx
@@ -12,84 +12,154 @@ import {
 	ContainerButton,
 } from "../RecoverPassword/styles";
 import { useState } from "react";
-import { Title } from "./styles";
+import { ButtonSubmit, PasswordText, Title } from "./styles";
+import axios from "axios";
+import { LoadingScreen } from "../../../Components/LoadingScreen";
+
+interface NewPasswordProps {
+	matricula: number;
+	novaSenha: string;
+	confirmacaoSenha: string;
+}
 
 export const NewPassword = () => {
 	const [passwordVisible, setPasswordVisible] = useState(false);
 	const [newPasswordVisible, setNewPasswordVisible] = useState(false);
 
+	const [isLoading, setIsLoading] = useState(false);
+	const [newPasswordForm, setNewPasswordForm] = useState<NewPasswordProps>({
+		matricula: 0,
+		novaSenha: "",
+		confirmacaoSenha: "",
+	});
+
+	async function PutNewPassword(
+		formMatricula: number,
+		formNovaSenha: string,
+		formConfirmacaoSenha: string
+	) {
+		setIsLoading(true);
+		const matricula = formMatricula;
+		const novaSenha = formNovaSenha;
+		const confirmacaoSenha = formConfirmacaoSenha;
+
+		await axios
+			.put(
+				"https://fc-services-server.onrender.com/alterar-senha/" + matricula,
+				{
+					novaSenha,
+					confirmacaoSenha,
+				}
+			)
+			.then(() => {
+				window.location.href = "/Login";
+			})
+			.finally(() => setIsLoading(false));
+	};
+
 	return (
-		<RecoverPasswordContainer>
-			<Link to="/login">
-				<BackButton
-					actionText={"Login"}
-					color="#AA0E27"
-					fontWeight={"600"}
+		<>
+			{isLoading === true ? <LoadingScreen /> : undefined}
+			<RecoverPasswordContainer>
+				<Link to="/login">
+					<BackButton
+						actionText={"Login"}
+						color="#AA0E27"
+						fontWeight={"600"}
+					/>
+				</Link>
+				<BoxEmpty
+					title="Identidade Confirmada!"
+					color="#68A439"
+					fontSize="16px"
+					icon={UserFound}
 				/>
-			</Link>
-			<BoxEmpty
-				// alt=""
-				// src=""
-				title="Identidade Confirmada!"
-				color="#68A439"
-				fontSize="16px"
-				icon={UserFound}
-			/>
-			<InputContainer>
-				<Title>Digite uma nova senha:</Title>
-				<InputLegend
-					legendText="Nova senha"
-					inputType={passwordVisible ? "text" : "password"}
-					maxLength={38}
-					minLength={8}
-					hasImage
-					source={passwordVisible ? EyeClosedIcon : EyeIcon}
-					// onChange={(e) => {
-					// 	setFormState({
-					// 		...formState,
-					// 		senha: e.target?.value,
-					// 	});
-					// }}
-					onClickImage={() => {
-						setPasswordVisible(!passwordVisible);
-					}}
-					placeholder="Digite sua senha"
-					height="56px"
-					width="auto"
-					border="1px solid #49454f"
-				/>
-				<InputLegend
-					legendText="Confirmar senha"
-					inputType={newPasswordVisible ? "text" : "password"}
-					maxLength={38}
-					minLength={8}
-					hasImage
-					source={newPasswordVisible ? EyeClosedIcon : EyeIcon}
-					// onChange={(e) => {
-					// 	setFormState({
-					// 		...formState,
-					// 		senha: e.target?.value,
-					// 	});
-					// }}
-					onClickImage={() => {
-						setNewPasswordVisible(!newPasswordVisible);
-					}}
-					placeholder="Digite sua senha"
-					height="56px"
-					width="auto"
-					border="1px solid #49454f"
-				/>
-			</InputContainer>
-			<ContainerButton>
-				<Button text="Alterar Senha" />
-				<Button
-					text="Voltar"
-					bg="transparent"
-					color="#635F60"
-					colorBorder="#635F60"
-					nextPage="/VerificacaoCodigo"
-				/>
-			</ContainerButton>
-		</RecoverPasswordContainer>
+				<InputContainer>
+					<Title>Digite uma nova senha:</Title>
+					<InputLegend
+						legendText="Nova senha"
+						inputType={passwordVisible ? "text" : "password"}
+						maxLength={38}
+						minLength={8}
+						hasImage
+						source={passwordVisible ? EyeClosedIcon : EyeIcon}
+						onChange={(e) => {
+							setNewPasswordForm({
+								...newPasswordForm,
+								novaSenha: e.target?.value,
+							});
+						}}
+						onClickImage={() => {
+							setPasswordVisible(!passwordVisible);
+						}}
+						placeholder="Digite sua senha"
+						height="56px"
+						width="auto"
+						border="1px solid #49454f"
+					/>
+					{newPasswordForm.novaSenha.length < 8 &&
+						newPasswordForm.novaSenha.length > 1 && (
+							<PasswordText>
+								A senha deve ter no mínimo 8 caracteres!
+							</PasswordText>
+						)}
+					<InputLegend
+						legendText="Confirmar senha"
+						inputType={newPasswordVisible ? "text" : "password"}
+						maxLength={38}
+						minLength={8}
+						hasImage
+						source={newPasswordVisible ? EyeClosedIcon : EyeIcon}
+						onChange={(e) => {
+							setNewPasswordForm({
+								...newPasswordForm,
+								confirmacaoSenha: e.target?.value,
+							});
+						}}
+						onClickImage={() => {
+							setNewPasswordVisible(!newPasswordVisible);
+						}}
+						placeholder="Digite sua senha"
+						height="56px"
+						width="auto"
+						border="1px solid #49454f"
+					/>
+					{newPasswordForm.confirmacaoSenha.length < 8 &&
+						newPasswordForm.confirmacaoSenha.length > 1 && (
+							<PasswordText>
+								A senha deve ter no mínimo 8 caracteres!
+							</PasswordText>
+						)}
+					{newPasswordForm.confirmacaoSenha !== "" &&
+						newPasswordForm.confirmacaoSenha.length >= 8 &&
+						newPasswordForm.confirmacaoSenha !== newPasswordForm.novaSenha && (
+							<PasswordText>
+								Digite a mesma senha em ambos os campos!
+							</PasswordText>
+						)}
+				</InputContainer>
+
+				<ContainerButton>
+					<ButtonSubmit
+						onClick={() => {
+							PutNewPassword(
+								Number(newPasswordForm.matricula),
+								newPasswordForm.novaSenha,
+								newPasswordForm.confirmacaoSenha
+							);
+							setIsLoading(true);
+						}}>
+						Alterar senha
+					</ButtonSubmit>
+					<Button
+						text="Voltar"
+						bg="transparent"
+						color="#635F60"
+						colorBorder="#635F60"
+						nextPage="/VerificacaoCodigo"
+					/>
+				</ContainerButton>
+			</RecoverPasswordContainer>
+		</>
 	);
 };
--- src/Pages/ForgotPassword/NewPassword/styles.ts
+++ src/Pages/ForgotPassword/NewPassword/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import theme from "../../../styles/theme";
 
 export const Title = styled.div`
 	font-family: Inter;
@@ -9,4 +10,33 @@ export const Title = styled.div`
 	letter-spacing: 0.01em;
 	text-align: left;
 	color: #5a8f19;
+`;
+
+export const PasswordText = styled.span`
+	font-family: "Roboto";
+	font-style: normal;
+	font-weight: 400;
+	font-size: 12px;
+	line-height: 16px;
+	color: #b3261e;
+`;
+
+interface ButtonSubmitProps {
+	color?: string;
+	bg?: string;
+	colorBorder?: string;
+}
+
+export const ButtonSubmit = styled.button<ButtonSubmitProps>`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background-color: ${({ bg }) => (bg ? bg : theme.colors.green[185])};
+	color: ${({ color }) => (color ? color : theme.colors.neutral.white)};
+	border-radius: 10rem;
+	border: ${({ colorBorder }) =>
+		colorBorder ? `1px solid ${colorBorder}` : "none"};
+	width: auto;
+	padding: 1.5rem 3rem;
+	font-size: 1.4rem;
 `;
