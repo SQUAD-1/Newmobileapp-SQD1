7ae70ef702c63da73323ff294504f00e2ae5e973
mateus.silva <mateus.silva@ferreiracosta.com.br>
2023-05-29 11:37:37 -0300

    "adicionando condicional aos ícones dos inputs"

--- src/Pages/Cadastro/index.tsx
+++ src/Pages/Cadastro/index.tsx
@@ -21,7 +21,7 @@ import EyeClosedIcon from "../Login/svg/eyeClosed.svg";
 import { InputLegend } from "../../Components/FildestInput";
 import { Modal } from "../../Components/Modal";
 import ClearIcon from "../../Assets/clear.svg";
-// import ClearDisabledIcon from "../../Pages/Login/svg/clearDisabled.svg";
+import ClearDisabledIcon from "../../Pages/Login/svg/clearDisabled.svg";
 
 interface UserRegisterProps {
 	matricula: number;
@@ -35,9 +35,9 @@ interface UserRegisterProps {
 }
 
 export const UserRegister = () => {
-  const [cargos, setCargos] = useState<{ nome: string }[]>([]);
+	const [cargos, setCargos] = useState<{ nome: string }[]>([]);
 	const [isLoading, setIsLoading] = useState(false);
-	
+
 	const [formState, setFormState] = useState<UserRegisterProps>({
 		matricula: 0,
 		nome: "",
@@ -102,6 +102,7 @@ export const UserRegister = () => {
 	}
 
 	const [passwordVisible, setPasswordVisible] = useState(false);
+	// const validEmail = /[a-zA-Z0-9._]+@[a-z0-9]+\.[a-z.]{2,}$/;
 
 	// const validEmail = /[a-zA-Z0-9._]+@[a-z0-9]+\.[a-z.]{2,}$/;
 	return (
@@ -134,7 +135,8 @@ export const UserRegister = () => {
 						border="1px solid #49454f"
 						width="auto"
 						hasImage
-						source={ClearIcon}
+						source={
+							formState.matricula < 5 ? ClearDisabledIcon :  ClearIcon}
 						imgDescription="icone de limpar"
 					/>
 					<InputLegend
@@ -151,7 +153,8 @@ export const UserRegister = () => {
 						border="1px solid #49454f"
 						width="auto"
 						hasImage
-						source={ClearIcon}
+						source={
+							formState.nome.length < 10 ? ClearDisabledIcon :  ClearIcon}
 						imgDescription="icone de limpar"
 					/>
 					<TitleInputArea>Qual sua filial?</TitleInputArea>
