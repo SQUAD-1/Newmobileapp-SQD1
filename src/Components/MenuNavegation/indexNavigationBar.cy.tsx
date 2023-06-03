21927c07dfda11856e09e6265e766c84732d8959
Wellington Braga <wellington.braga@ferreiracosta.com.br>
2023-06-03 14:38:31 -0300

    Adição do Cypress no Projeto

---
+++ src/Components/MenuNavegation/indexNavigationBar.cy.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { NavigationBar } from './index'
+
+describe('<NavigationBar />', () => {
+  it('renders', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(<NavigationBar />)
+  })
+})
