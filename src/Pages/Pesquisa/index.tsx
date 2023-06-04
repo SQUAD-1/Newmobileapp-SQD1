27ac0271656fd12f3f25f5a20a0e5b4e5db474a9
Wellington Braga <wellington.braga@ferreiracosta.com.br>
2023-06-04 17:13:55 -0300

    feat: :sparkles: Conexão com a API de pesquisa de chamado

--- src/Pages/Pesquisa/index.tsx
+++ src/Pages/Pesquisa/index.tsx
@@ -1,12 +1,14 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { verificarLogin } from "../../Assets/Utils";
 import { IssueMobile } from "../../Components/Home/CalledMobile";
 import { HeaderMobile } from "../../Components/Home/HeaderMobile";
 import { NavigationBar } from "../../Components/MenuNavegation";
 import Searchbar from "../../Components/Searchbar";
 import { searchResults } from "./data";
+import { api } from "../../Services";
 import { api } from "../../Services";
 import {
 	FlexContainer,
@@ -15,11 +17,12 @@ import {
 	SearchPageContainer,
 } from "./styles";
 import { IssueDto, IssueMobileProps } from "../../Assets";
-import { LoadingScreen } from "../../Components/LoadingScreen";
-import { BoxEmptyContainer } from "../Home/styles";
-import { BoxEmpty } from "../../Components/BoxEmpty";
 
 const Pesquisa = () => {
+	const [isLoading, setIsLoading] = useState(false);
+	const [searchResults, setSearchResults] = useState<IssueDto[]>([]);
+	const [search, setSearch] = useState("");
+
 	const [isLoading, setIsLoading] = useState(false);
 	const [searchResults, setSearchResults] = useState<IssueDto[]>([]);
 	const [search, setSearch] = useState("");
@@ -33,12 +36,12 @@ const Pesquisa = () => {
 	verificarLogin();
 
 	useEffect(() => {
+		setIsLoading(true);
 		search && searchUserIssue(search);
-		// eslint-disable-next-line react-hooks/exhaustive-deps
+	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [search]);
 
 	const searchUserIssue = (issueString: string) => {
-		setIsLoading(true);
 		api
 			.get(
 				`FiltroChamado?matricula=${usuarioLogado.matricula}&nome=${issueString}`,
@@ -51,14 +54,10 @@ const Pesquisa = () => {
 				}
 			)
 			.then((response) => {
-				setIsLoading(false);
 				setSearchResults(response.data);
 			})
 			.catch((error) => {
 				console.log(error);
-			})
-			.finally(() => {
-				setIsLoading(false);
 			});
 	};
 
@@ -68,35 +67,21 @@ const Pesquisa = () => {
 			<PageContainer>
 				<SearchContainer>
 					<Searchbar getInputValue={(value: string) => setSearch(value)} />
+					<Searchbar getInputValue={(value: string) => setSearch(value)} />
 				</SearchContainer>
 				<MainContainer>
-					{isLoading ? (
-						<LoadingScreen />
-					) : (
-						<>
-							{searchResults.length === 0 ? (
-								<BoxEmptyContainer>
-									<BoxEmpty
-										title="Nenhum chamado encontrado"
-									/>
-								</BoxEmptyContainer>
-							) : (
-								<></>
-							)}
-							{searchResults.map((issue) => {
-								return (
-									<IssueMobile
-										key={issue.idChamado}
-										id={issue.idChamado.toString()}
-										nome={issue.nome}
-										status={issue.status}
-										date={issue.dataRelato}
-										isUpdated={true}
-									/>
-								);
-							})}
-						</>
-					)}
+					{searchResults.map((issue) => {
+						return (
+							<IssueMobile
+								key={issue.idChamado}
+								id={issue.idChamado.toString()}
+								nome={issue.nome}
+								status={issue.status}
+								date={issue.dataRelato}
+								isUpdated={true}
+							/>
+						);
+					})}
 				</MainContainer>
 			</PageContainer>
 			<NavigationBar />
