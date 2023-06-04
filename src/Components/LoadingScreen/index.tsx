5586062cb962c2a02e3b9ded83d2a4518eecf8c0
Wellington Braga <wellington.braga@ferreiracosta.com.br>
2023-06-04 05:17:31 -0300

    fix: :lipstick: Correção dos estilos das telas Home, Search e Request

---
+++ src/Components/LoadingScreen/index.tsx
@@ -0,0 +1,16 @@
+import { Container } from "./styles";
+
+export const LoadingScreen = () => {
+	return (
+		<Container>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+		</Container>
+	);
+};
