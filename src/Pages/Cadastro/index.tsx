819df11653017c73f72d3b28c08fcff962e53f09
Larissa Ferreira <larissa.ferreira@ferreiracosta.com.br>
2023-06-05 10:37:15 -0300

    Correção de bugs no fluxo

--- src/Pages/Cadastro/index.tsx
+++ src/Pages/Cadastro/index.tsx
@@ -23,6 +23,7 @@ import { Modal } from "../../Components/Modal";
 import ClearIcon from "../../Assets/clear.svg";
 import ClearDisabledIcon from "./svg/clearDisabled.svg";
 import { api } from "../../Services";
+import { api } from "../../Services";
 
 interface UserRegisterProps {
 	matricula: string;
@@ -38,8 +39,7 @@ interface UserRegisterProps {
 export const UserRegister = () => {
 	const [cargos, setCargos] = useState<{ nome: string }[]>([]);
 	const [isLoading, setIsLoading] = useState(false);
-	const [responseMenssage, setResponseMenssage] = useState("");
-	console.log(responseMenssage);
+	const [responseMessage, setResponseMessage] = useState("");
 
 	const [formState, setFormState] = useState<UserRegisterProps>({
 		matricula: "",
@@ -56,7 +56,7 @@ export const UserRegister = () => {
 	const validEmail = /[a-zA-Z0-9._]+@[a-z0-9]+\.[a-z.]{2,}$/;
 
 	const isDisabledButton =
-		validEmail.test(formState.email) && formState.senha.length >= 5;
+		validEmail.test(formState.email) && formState.senha.length > 7;
 
 	const verifyModal = () => {
 		if (!openModal) {
@@ -89,6 +89,8 @@ export const UserRegister = () => {
 
 		api
 			.post("/CadastrarUsuario/", {
+		api
+			.post("/CadastrarUsuario/", {
 				matricula,
 				nome,
 				funcao,
@@ -103,7 +105,7 @@ export const UserRegister = () => {
 			})
 			.catch((response) => {
 				console.log(response, response.response.data);
-				setResponseMenssage(response.response.data);
+				setResponseMessage(response.response.data);
 			})
 			.finally(() => {
 				setIsLoading(false);
@@ -153,8 +155,8 @@ export const UserRegister = () => {
 							setFormState({ ...formState, matricula: "" });
 						}}
 					/>
-					{(responseMenssage === "A matrícula já está cadastrada." ||
-						responseMenssage ===
+					{(responseMessage === "A matrícula já está cadastrada." ||
+						responseMessage ===
 							"O email e a matrícula já estão cadastrados.") && (
 						<PasswordText>Matricula já cadastrada.</PasswordText>
 					)}
@@ -292,9 +294,9 @@ export const UserRegister = () => {
 						<PasswordText>Inserira um email válido</PasswordText>
 					)}
 					{validEmail.test(formState.email) &&
-						(responseMenssage ===
+						(responseMessage ===
 							"O email e a matrícula já estão cadastrados." ||
-							responseMenssage === "O email já está cadastrado.") && (
+							responseMessage === "O email já está cadastrado.") && (
 							<PasswordText>Email já cadastrado.</PasswordText>
 						)}
 					<InputLegend
