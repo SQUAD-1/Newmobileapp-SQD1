5ce8d6f14616c9cd8d8f342f13b9d4b2efc485f1
Cleyton Cabral <cleyton.cabral@ferreiracosta.com.br>
2023-06-08 18:06:47 -0300

    adião de ícones

--- src/Components/IconesDeStatus/styles.ts
+++ src/Components/IconesDeStatus/styles.ts
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const IconeDeStatusContainer = styled.section`
 	position: fixed;
 	display: flex;
+	align-items: center;
 	flex-direction: column;
 	gap: 2rem;
 	right: 4rem;
